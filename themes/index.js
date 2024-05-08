54d2fa0b5f05344fbfccb82d9c7ad15af16529f9
𝙅𝙖𝙨𝙪𝙧𝙗𝙚𝙠 𝙃𝙖𝙮𝙙𝙖𝙧𝙤𝙫 <157284471+jasurhaydarovcode@users.noreply.github.com>
2024-05-08 23:49:33 +0500

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -462,6 +462,12 @@ export const themes = {
     icon_color: "ffffff",
     bg_color: "35,4158d0,c850c0,ffcc70",
   },
+  khaydarov_gradient: {
+    title_color: "ffffff",
+    text_color: "ffffff",
+    icon_color: "ffffff",
+    bg_color: "35,4158d0,c850c0,ff0000",
+  },
 };
 
 export default themes;
